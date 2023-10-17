b851160186462e5d7883873be921286412321a2f
agislashed <anaisbvro@gmail.com>
2023-10-17 15:38:12 +0200

    add : logo qui bouge

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+// code pour le mouvement 360° du logo
 
+document.addEventListener("DOMContentLoaded", function() {
+    const logo = document.querySelector(".logo");
+    logo.addEventListener("click", function() {
+        logo.classList.add("clicked");
+
+        // Réinitialisez la classe "clicked" après l'animation (en utilisant un délai)
+        setTimeout(function() {
+            logo.classList.remove("clicked");
+        }, 300); // 300ms (0.3s) correspond à la durée de l'animation dans les styles CSS
+    });
+});
