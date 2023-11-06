205e6e41abca8f8e98ba9a3b0dea1c2217bd60ae
agislashed <anaisbvro@gmail.com>
2023-11-06 14:29:31 +0100

    add essai de javascript sur choix avatar

--- main.js
+++ main.js
@@ -84,3 +84,15 @@ document.addEventListener('mousemove', function(event) {
         renderer.render(scene, camera);
     }
 })
+
+
+// Sélection avatar 
+
+const avatars = document.querySelectorAll('.avatar');
+
+avatars.forEach(avatar => {
+    avatar.addEventListener('click', () => {
+        avatars.forEach(a => a.classList.remove('selected'));
+        avatar.classList.add('selected');
+    });
+});
